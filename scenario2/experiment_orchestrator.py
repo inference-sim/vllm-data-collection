7c18415ad1110f0fc11e937ce57db2587ec565db
Dipanwita Guhathakurta <dipanwitaguhathakurta@wecm-9-67-20-180.wecm.ibm.com>
2025-09-03 20:31:49 -0400

    Fixed m=0 issue

--- scenario2/experiment_orchestrator.py
+++ scenario2/experiment_orchestrator.py
@@ -112,8 +112,7 @@ def main():
     # models = ["ibm-granite/granite-3.3-8b-instruct", "mistralai/Mistral-Small-24B-Instruct-2501"]
     # models = ["Qwen/Qwen3-32B"]
     num_runs = 1
-    # models = ["Qwen/Qwen2.5-0.5B", "Qwen/Qwen2.5-1.5B", "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-7B", "mistralai/Mistral-7B-Instruct-v0.1", "google/gemma-7b", "meta-llama/Llama-3.1-8B","ibm-granite/granite-3.3-8b-instruct", "Qwen/Qwen3-14B", "mistralai/Mistral-Small-24B-Instruct-2501", "Qwen/Qwen3-32B"]
-    models = ["mistralai/Mistral-7B-Instruct-v0.1", "google/gemma-7b", "meta-llama/Llama-3.1-8B", "mistralai/Mistral-Small-24B-Instruct-2501"]
+    models = ["Qwen/Qwen2.5-0.5B", "Qwen/Qwen2.5-1.5B", "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-7B", "mistralai/Mistral-7B-Instruct-v0.1", "google/gemma-7b", "meta-llama/Llama-3.1-8B","ibm-granite/granite-3.3-8b-instruct", "Qwen/Qwen3-14B", "mistralai/Mistral-Small-24B-Instruct-2501", "Qwen/Qwen3-32B"]
 
     for run in range(num_runs):
         for idx, model in enumerate(models):
@@ -121,9 +120,9 @@ def main():
             remote_exp_folder = f"{time.strftime('%Y%m%d-%H%M%S')}_{benchmark_name}"
             for mode in modes:
                 run_experiment(model, mode, remote_exp_folder)
-            # if idx < 4:
-            #     time.sleep(360)
-            if idx <= 2:
+            if idx < 4:
+                time.sleep(360)
+            elif idx >=4 and idx <=7:
                 time.sleep(600)
             else:
                 time.sleep(1200)
