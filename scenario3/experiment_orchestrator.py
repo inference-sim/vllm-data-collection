fa4d7d122aefbf60d14b1086517311e3699c999a
susiejojo <susiejojo1@gmail.com>
2025-10-22 14:43:37 -0400

    More conflicts with scenario3

--- scenario3/experiment_orchestrator.py
+++ scenario3/experiment_orchestrator.py
@@ -110,7 +110,7 @@ def run_experiment(model, mode, remote_exp_folder: str):
 def main():
     num_runs = 1
     # models = ["meta-llama/Llama-3.1-8B"]
-    models = ["Qwen/Qwen2.5-0.5B", "Qwen/Qwen2.5-1.5B", "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-7B", "google/gemma-7b", "mistralai/Mistral-7B-Instruct-v0.1", "meta-llama/Llama-3.1-8B", "ibm-granite/granite-3.3-8b-instruct", "Qwen/Qwen3-14B", "mistralai/Mistral-Small-24B-Instruct-2501", "Qwen/Qwen3-32B"]
+    models = ["Qwen/Qwen2.5-0.5B", "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-7B", "mistralai/Mistral-7B-Instruct-v0.1", "google/gemma-7b", "meta-llama/Llama-3.1-8B","ibm-granite/granite-3.3-8b-instruct", "Qwen/Qwen3-14B", "mistralai/Mistral-Small-24B-Instruct-2501", "Qwen/Qwen3-32B"]
 
     for run in range(num_runs):
         for idx, model in enumerate(models):
