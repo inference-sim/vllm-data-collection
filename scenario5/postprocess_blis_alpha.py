25a8e1a94bc25c90ddb74dd8545ed854c928534a
susiejojo <susiejojo1@gmail.com>
2025-11-10 14:47:37 -0500

    Working alpha model training script

--- scenario5/postprocess_blis_alpha.py
+++ scenario5/postprocess_blis_alpha.py
@@ -21,28 +21,32 @@ def calculate_metrics(X_train, y_train, alpha_model):
 
 def get_delays(filepath):
     all_requests = []
-    with open(filepath, 'r') as file:
-        traces_raw_data = json.load(file)
-        for resourceSpan in traces_raw_data["resourceSpans"]:
-            request = {}
+    traces_raw_data = []
+    with open(filepath, 'r', encoding='utf-8') as f:
+        for line in f:
+            json_object = json.loads(line.strip())
+            traces_raw_data.append(json_object)
+    for data in traces_raw_data:
+        for resourceSpan in data["resourceSpans"]:
             for scopeSpan in resourceSpan["scopeSpans"]:
                 for span in scopeSpan["spans"]:
+                    request = {}
                     for attribute in span["attributes"]:
                         if attribute["key"] == "gen_ai.request.id":
-                            request["request_id"] = attribute["value"]
+                            request["request_id"] = attribute["value"]["stringValue"]
                         if attribute["key"] == "gen_ai.usage.prompt_tokens":
-                            request["input_tokens"] = attribute["value"]
+                            request["input_tokens"] = int(attribute["value"]["intValue"])
                         if attribute["key"] == "gen_ai.usage.completion_tokens":
-                            request["output_tokens"] = attribute["value"]
+                            request["output_tokens"] = int(attribute["value"]["intValue"])
                         if attribute["key"] == "gen_ai.latency.e2e":
-                            request["e2e_latency"] = attribute["value"]
+                            request["e2e_latency"] = attribute["value"]["doubleValue"]
                         if attribute["key"] == "gen_ai.latency.time_in_queue":
-                            request["queued_time"] = attribute["value"]
+                            request["queued_time"] = attribute["value"]["doubleValue"]
                         if attribute["key"] == "gen_ai.latency.time_in_model_prefill":
-                            request["prefill_time"] = attribute["value"]
+                            request["prefill_time"] = attribute["value"]["doubleValue"]
                         if attribute["key"] == "gen_ai.latency.time_in_model_decode":
-                            request["decode_time"] = attribute["value"]
-                all_requests.append(request)
+                            request["decode_time"] = attribute["value"]["doubleValue"]
+                    all_requests.append(request)
     return all_requests
 
 def train_alpha_model(all_requests):
@@ -57,10 +61,11 @@ def train_alpha_model(all_requests):
     calculate_metrics(input_lengths, processing_times, alpha_model)
 
 parser = argparse.ArgumentParser(description="Read and parse traces JSON file.")
-parser.add_argument("traces_filepath", help="Path to the vllm traces file to be read.")
+parser.add_argument("--traces_filepath", help="Path to the vllm traces file to be read.")
 args = parser.parse_args()
 
 all_requests = get_delays(args.traces_filepath)
+print(len(all_requests))
 train_alpha_model(all_requests)
 
 
