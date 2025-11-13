ac517570b018dd0f67f6e5051fc940c674197e99
susiejojo <susiejojo1@gmail.com>
2025-11-13 18:23:41 -0500

    Common vs distinct

--- scenario5/postprocess_guidellm_common.py
+++ scenario5/postprocess_guidellm_common.py
@@ -2,25 +2,6 @@ import argparse
 import json
 import os
 import sys
-import yaml
-
-def construct_BLIS_reqgenconfig(guidellm_profile, rps):
-    """
-    Given GuideLLM profile and request rates,
-    construct BLIS-style request gen-config file.
-    """
-    blis_reqgen_config = {
-        "format": "GuideLLM",
-        "seed": guidellm_profile["random-seed"], 
-        "rate": []
-    }
-    blis_reqgen_config["rate"] = {
-        "arrival-type": "Constant",
-        "rate": rps,
-        "max-requests": guidellm_profile["max-requests"]
-    }
-    blis_reqgen_config["data"] = guidellm_profile["data"]
-    return blis_reqgen_config
 
 def get_GuideLLM_rps_list(guidellm_results):
     """
@@ -59,18 +40,12 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Read and parse traces JSON file.")
     parser.add_argument("--guidellm_results_file", 
                         help="Path to the GuideLLM JSON output file to be read.")
-    parser.add_argument("--guidellm_profile_file", 
-                        help="Path to the GuideLLM YAML profile file to be read.")
-    parser.add_argument("--blis_reqgen_config_folder",
-                        default="blis_reqgenconfigs",
-                        help="Folder to save BLIS' YAML request gen-config file")
     parser.add_argument("--sweep_info_path",
                         default=".", 
                         help="Location to save GuideLLM's sweep trial info")
     args = parser.parse_args()
 
     guidellm_results_filepath = args.guidellm_results_file
-    guidellm_profile_filepath = args.guidellm_profile_file
     sweep_info_filepath = os.path.join(args.sweep_info_path, "sweep_info.json")
 
     # read GuideLLM results file
@@ -90,26 +65,3 @@ if __name__=="__main__":
     # Save sweep info to JSON file
     with open(sweep_info_filepath, 'w+') as f:
         json.dump(sweep_info, f, indent=4)
-
-    # read GuideLLM profile file
-    try:
-        with open(guidellm_profile_filepath, 'r') as f:
-            guidellm_profile = yaml.safe_load(f)
-    except:
-        print("Could not read GuideLLM profile file.")
-        sys.exit()
-    
-    # Check if args.blis_reqgen_config_folder exists, otherwise create
-    os.makedirs(args.blis_reqgen_config_folder, exist_ok=True)
-
-    # Construct BLIS-style request gen-config
-    for rps in rps_list:
-        blis_reqgen_config = construct_BLIS_reqgenconfig(guidellm_profile, rps)
-        blis_reqgen_config_filename = os.path.join(
-            args.blis_reqgen_config_folder, 
-            f"requestgenconfig_RPS={round(rps, 3)}.yaml"
-        )
-
-        # Save to YAML file - one per RPS
-        with open(blis_reqgen_config_filename, 'w+') as f:
-            yaml.dump(blis_reqgen_config, f)
