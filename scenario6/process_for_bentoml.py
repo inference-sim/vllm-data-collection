e3428fa298e7a376ca1660fea6c2233ae23c109e
susiejojo <susiejojo1@gmail.com>
2025-12-10 17:15:43 -0500

    Added side-by-side plots

--- scenario6/process_for_bentoml.py
+++ scenario6/process_for_bentoml.py
@@ -10,11 +10,29 @@ METRICS_TO_COMPARE = ["ttft", "itl", "e2e"]
 def plot_vllm_vs_sim(errors, plot_title=""):
     overall_errors = {}
     for metric in METRICS_TO_COMPARE:
-        metric_error = f"{metric} MAPE"
+        metric_error = f"{metric}_MAPE"
         overall_errors[metric_error] = errors[metric_error].mean()
     plt.figure(figsize=(10, 6))
     colors = ['orange', 'red', 'green', 'blue', 'purple', 'yellow']
     plt.bar(list(overall_errors.keys()), list(overall_errors.values()), label=list(overall_errors.keys()), color=colors)
+    
+    plt.title(f'MAPE error - vllm vs sim - {plot_title}')
+    plt.xlabel("Metrics")
+    plt.ylabel("Error %")
+    plt.legend()
+    plots_folder = f"test_plots/bentoml"
+    os.makedirs(plots_folder, exist_ok=True)
+    plt.savefig(f'{plots_folder}/{plot_title}_error.png')
+
+    for metric in METRICS_TO_COMPARE:
+        plt.figure(figsize=(10, 6))
+        plt.plot(errors[f"{metric}_real"], color = "blue")
+        plt.plot(errors[f"{metric}_pred"], color = "red")
+        plt.title(f'Real vs BentoML - {metric.upper()}')
+        plt.xlabel("Exp idx")
+        plt.ylabel("Error %")
+        plt.legend(["Real", "Predicted - BentoML"])
+        plt.savefig(f'{plots_folder}/bentoml_{metric.upper()}.png')
     
     plt.title(f'MAPE error - vllm vs sim - {plot_title}')
     plt.xlabel("Metrics")
@@ -30,7 +48,9 @@ def test_bento():
         data = yaml.safe_load(f)
     df = df[(df["train_test"] == "test") & (df["saturated"] == False)]
     df = df.groupby(["model_hf_repo", "hardware", "hardware_count", "prompt_tokens", "output_tokens"]).first().reset_index() # take only the first request rate for a group
-    errors = {"ttft MAPE": [], "itl MAPE": [], "e2e MAPE": []}
+    errors = {"ttft_real": [], "itl_real": [], "e2e_real": [],
+              "ttft_pred": [], "itl_pred": [], "e2e_pred": [],
+              "ttft_MAPE": [], "itl_MAPE": [], "e2e_MAPE": []}
 
     for idx, row in enumerate(df.itertuples()):
         llm = row.model_hf_repo
@@ -60,14 +80,20 @@ def test_bento():
             
                 ttft = get_metric("TTFT", r"TTFT:\s*([\d\.]+)", result.stdout)
                 itl = get_metric("ITL", r"ITL:\s*([\d\.]+)", result.stdout)
-                e2e = get_metric("E2E", r"E2E:\s*([\d\.]+)", result.stdout)
+                e2e = get_metric("E2E", r"E2E:\s*([\d\.]+)", result.stdout) * 1e3
 
                 print(f"TTFT: {ttft}")
                 print(f"ITL: {itl}")
                 print(f"E2E: {e2e}")
-                errors["ttft MAPE"].append(abs(ttft - row.ttft_mean)/row.ttft_mean * 100)
-                errors["itl MAPE"].append(abs(itl - row.itl_mean)/row.itl_mean * 100)
-                errors["e2e MAPE"].append(abs(e2e - row.e2e_mean)/row.e2e_mean * 100)
+                errors["ttft_real"].append(row.ttft_mean)
+                errors["itl_real"].append(row.itl_mean)
+                errors["e2e_real"].append(row.e2e_mean)
+                errors["ttft_pred"].append(ttft)
+                errors["itl_pred"].append(itl)
+                errors["e2e_pred"].append(e2e)
+                errors["ttft_MAPE"].append(abs(ttft - row.ttft_mean)/row.ttft_mean * 100)
+                errors["itl_MAPE"].append(abs(itl - row.itl_mean)/row.itl_mean * 100)
+                errors["e2e_MAPE"].append(abs(e2e - row.e2e_mean)/row.e2e_mean * 100)
             except Exception as e:
                 print("Failed to run BentoML", cmd_str, e)
                 pass
